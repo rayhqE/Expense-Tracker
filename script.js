7082329f3737512ac3b92a418ffb38bf916e42f8
Rayyan <rayhq3@gmail.com>
2026-01-13 00:07:57 +0530

    extracting value and type checking

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const expenseForm = document.getElementById("expense-form");
+  const expenseNameInput = document.getElementById("expense-name");
+  const expenseAmountInput = document.getElementById("expense-amount");
+  const expenseList = document.getElementById("expense-list");
+  const totalAmountDisplay = document.getElementById("total-amount");
+
+  let expenses = [];
+  let totalAmount = calculateTotal();
+  expenseForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = expenseNameInput.value.trim();
+    const amount = parseFloat(expenseAmountInput.value.trim());
+
+    if (name !== "" && !isNaN(amount) && amount > 0) {
+      const newExpense = {
+        id: Date.now(),
+        name,
+        amount,
+      };
+
+      expenses.push(newExpense);
+    }
+  });
+
+  function calculateTotal() {}
+});
